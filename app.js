2d9ee1d9466487dd4489f66e720fca85ca505935
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-01-30 12:45:34 -0500

    render players list names by creating a functiong that use a foreach to go to each element and help me to append data from ol to li

--- app.js
+++ app.js
@@ -2,24 +2,46 @@
 
 // 2) Create state ✅
 
-const state ={
+const state = {
+  playersList:[],
 
-playersList: [],
-
-}
-
+};
 
 // 1) Create a fetch ✅
 
-const getDataApi = async () =>{
-  const res = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2501-FTB-ET-WEB-FT/players`);
-  const players = await res.json();
-  // console.log(x);//testing
-  state.playersList = players;
+const getDataApi = async () => {
+  const res = await fetch(
+    `https://fsa-puppy-bowl.herokuapp.com/api/2501-FTB-ET-WEB-FT/players`
+  );
+  const response = await res.json();
+  console.log(response); //testing
+  state.playersList = response.data.players;
+
+  renderPlayersList();
+
 };
 
+
+//- 3) Create a display -> Render function✅
+
+const renderPlayersList = () => {
+
+  ol = document.querySelector("ol");
+
+  ol.innerHTML = '';
+
+  state.playersList.forEach((player) => {
+
+    const li =document.createElement("li");
+    li.innerHTML = `Players :  ${player.name} `; //  Breed: ${player.breed}, Status: ${player.status}
+    // console.log(ol);
+    ol.append(li);
+
+  });
+  
+
+};
+
+
+
 getDataApi();
-
-
-
-
